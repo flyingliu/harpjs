35ea2d862dc56e04a65aa219936de4be317c5f8b
Ricardo <linuxmei@foxmail.com>
2016-07-22 18:17:17 +0800

    a:

--- public/vue/js/hello.js
+++ public/vue/js/hello.js
@@ -92,7 +92,7 @@ var vm = new Vue({
             get: function() {
                 return this.firstName + ' ' + this.lastName
             },
-            set:: function(newValue) {
+            set: function(newValue) {
                 var names = newValue.split(' ')
                 this.firstName = names[0]
                 this.lastName = names[names.length - 1]
